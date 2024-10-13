9267ea305f7238a3b30af5898adaab5224c8a923
taquan <tnaquan@gmail.com>
2024-10-13 15:22:05 +0700

    Add home template

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('register/', views.register_view, name="register"),
     path('create_blog/', views.create_blog_view, name="create"),
-    path('blog_list/', views.blog_list_view, name="read"),
+    path('blog_list/', views.blog_list_view, name="blog_list"),
     path('update_blog/<int:id>', views.update_blog_view, name="update"),
     path('delete_blog/<int:id>', views.delete_blog_view, name="delete"),
 ]
