52715b17ae99f79af889196bc926eb00a6fba1ac
taquaan <[tnaquan@gmail.com]>
2024-10-16 14:50:49 +0800

    Add session-based authentication

--- blogProject/blogApp/api/viewsets.py
+++ blogProject/blogApp/api/viewsets.py
@@ -1,66 +1,108 @@
 from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializer import BlogSerializer
+from .serializer import BlogSerializer, UserSerializer
 from ..models import Blog
+from django.contrib.auth.models import User
 
-# API
-# Get all blogs - Create a new blog
+# GET/POST BLOGS
 class ListCreateBlogView(ListCreateAPIView):
     model = Blog
     serializer_class = BlogSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     
-    # Specify the model instance to query
     def get_queryset(self):
         return Blog.objects.all()
     
+    # HANDLE BLOG CREATE
     def create(self, request, *args, **kwargs):
         serializer = BlogSerializer(data=request.data)
         
         if serializer.is_valid():
             serializer.save()
-            
             return JsonResponse({
                 'message': 'New Blog created successfully'
             }, status=status.HTTP_201_CREATED)
-        
         return JsonResponse({
             'message': 'Invalid Blog credentials. Please try again'
         }, status=status.HTTP_400_BAD_REQUEST)
         
-
-# Update a blog - delete a blog
+# UPDATE/DELETE BLOG
 class UpdateDeleteBlogView(RetrieveUpdateDestroyAPIView):
     model = Blog
     serializer_class = BlogSerializer
     lookup_field = "blog_id"
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         return Blog.objects.all()
     
-    # HANDLE UPDATE
+    # HANDLE BLOG UPDATE
     def put(self, request, *args, **kwargs):
-        # Get the blog about to get updated
         blog = self.get_object() 
         serializer = BlogSerializer(blog, data=request.data)
-        # Update that blog with the request data
         
         if serializer.is_valid():
             serializer.save()
-            
             return JsonResponse({
                 'message': 'Update Blog Successfully'
             }, status = status.HTTP_200_OK)
-
         return JsonResponse({
             'message': 'Update Blog Unsuccessful'
         }, status = status.HTTP_400_BAD_REQUEST)
         
-    # HANDLE DELETE
+    # HANDLE BLOG DELETE
     def delete(self, request, *args, **kwargs):
         blog = self.get_object()
         blog.delete()
-        
         return JsonResponse({
             'message': 'Blog Deleted Successfully'
         }, status=status.HTTP_200_OK)
+        
+# GET/POST USERS
+class ListCreateUserView(ListCreateAPIView):
+    model = User
+    serializer_class = UserSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAdminUser]
+    
+    def get_queryset(self):
+        return User.objects.all()
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            return JsonResponse({"message": "User created successfully"}, status = status.HTTP_200_OK)
+        return JsonResponse({"message": "Invaid credentials"}, status = status.HTTP_400_BAD_REQUEST)
+
+# UPDATE/DELETE USER
+class UpdateDeleteUserView(RetrieveUpdateDestroyAPIView):
+    model = User
+    serializer_class = UserSerializer
+    lookup_field = "id"
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAdminUser]
+    
+    def get_queryset(self):
+        return User.objects.all()
+    
+    def put(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = UserSerializer(user, data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'message': 'User updated successfully'}, status = status.HTTP_200_OK)
+        return JsonResponse({'message': 'Invaid User Credentials'}, status = status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.delete()
+        return JsonResponse({'message': 'User deleted successfully'}, status = status.HTTP_200_OK)
+        
