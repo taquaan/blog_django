a37cfe8e42a4476465377efb788643846dafb6ab
taquaan <[tnaquan@gmail.com]>
2024-10-21 09:57:48 +0800

    Add handling remove and update categories

--- blogProject/static/js/createBlog.js
+++ blogProject/static/js/createBlog.js
@@ -1,3 +1,4 @@
+// CKEditor5 config
 const config = {
   toolbar: [
     "heading",
@@ -9,25 +10,36 @@ const config = {
     "numberedList",
     "blockQuote",
     "|",
-    "code",
-    "subscript",
-    "superscript",
-    "highlight",
-    "|",
-    "codeBlock",
-    "sourceEditing",
-    "todoList",
-    "|",
-    "fontSize",
-    "fontFamily",
-    "fontColor",
-    "fontBackgroundColor",
-    "removeFormat",
     "insertTable",
   ],
 };
+
 window.ClassicEditor.create(document.querySelector(".content"), config).catch(
   (error) => {
     console.error(error);
   }
 );
+
+// Handling categories
+const categoryField = document.querySelector(".cate-field");
+const templateField = categoryField.cloneNode(true);
+
+document.querySelector(".add-new-cate").addEventListener("click", (e) => {
+  e.preventDefault();
+  const cloneField = templateField.cloneNode(true);
+  document.querySelector(".cate-list").appendChild(cloneField);
+
+  // HANDLING DELETE CATEGORY
+  document.querySelectorAll(".delete-btn").forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const removedCate = deleteBtn.closest(".cate-field");
+      console.log(removedCate);
+      if (removedCate) {
+        removedCate.remove();
+      } else {
+        console.error("There has been an error deleting a category");
+      }
+    });
+  });
+});
